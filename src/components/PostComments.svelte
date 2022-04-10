592e10eec5add21449cf17cdb2aa2ea72e83aafe
Raphael Alvarenga <raphael.alvarenga@tinnova.com.br>
2022-04-09 22:02:32 -0300

    Created a concept for a post

--- src/components/PostComments.svelte
+++ src/components/PostComments.svelte
@@ -1,7 +1,35 @@
 <div class="comments">
     <div class="comment">
-        <div class="avatar">Avatar</div>
-        <div class="author">Ringo Starr</div>
-        <div class="description">Description</div>
+        
+        <div class="author">
+            <div class="avatar">
+            <img src="https://studiosol-a.akamaihd.net/uploadfile/letras/fotos/2/1/d/0/21d077ea5a8c65c86ef95a9e5b598b69.jpg" alt="user">
+        </div>
+        Ringo Starr</div>
+        <div class="description">This is actually a great news! I can't wait to see what is going to happen from that...</div>
     </div>
 </div>
+
+<style>
+    /* .comments {} */
+
+    .author {
+        display: flex;
+        align-items: center;
+    }
+
+    .avatar {
+        width: 50px;
+        border-radius: 25px;
+        overflow: hidden;
+        margin-right: 10px;
+    }
+
+    .avatar img {
+        width: 50px;
+    }
+
+    .description {
+        margin-left: 60px;
+    }
+</style>
--- src/components/PostLikes.svelte
+++ src/components/PostLikes.svelte
@@ -1,1 +1,7 @@
 <div class="likes"><div>Like</div></div>
+
+<style>
+    .likes {
+        padding: 30px 0;
+    }
+</style>
