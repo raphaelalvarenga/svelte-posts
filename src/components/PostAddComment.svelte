81323c2218fa649f0dcf4b6b2dd6c5125b64ae47
Raphael Alvarenga <raphaelalvarenga2@gmail.com>
2022-04-13 23:45:04 -0300

    Refactored PostAddComment

--- src/components/PostAddComment.svelte
+++ src/components/PostAddComment.svelte
@@ -4,7 +4,7 @@
 
 <div style = "display: flex; align-items: center; margin-top: 30px;">
     <div><input type="text" placeholder="Type something..."></div>
-    <div style = "width: 30px; transform: rotateZ(-20deg); margin-top: -10px"><MdSend /></div>
+    <div class="send-container"><MdSend /></div>
 </div>
 
 <style>
@@ -12,5 +12,13 @@
         margin-right: 10px;
         border: 2px solid rgba(0, 0, 0, 0.5);
         border-radius: 20px;
+        height: 30px;
+        padding-left: 10px;
+    }
+
+    .send-container {
+        width: 30px;
+        transform: rotateZ(-20deg);
+        margin-top: -10px;
     }
 </style>
